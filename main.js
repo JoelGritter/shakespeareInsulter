4d2cb1f08a2a15e20e591028e83c26263d7faa6d
Joel Gritter <JoelGritter@users.noreply.github.com>
2017-04-19 16:52:36 -0400

    Upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var columnOne = [ 'artless','bawdy','beslubbering','bootless','churlish','cockered','clouted','craven','currish','dankish','dissembling','droning','errant','fawning','fobbing','froward','frothy','gleeking','goatish','gorbellied','impertinent','infectious','jarring','loggerheaded','lumpish','mammering','mangled','mewling','paunchy','pribbling','puking','puny','qualling','rank','reeky','roguish','ruttish','saucy','spleeny','spongy','surly','tottering','unmuzzled','vain','venomed','villainous','warped','wayward','weedy','yeasty' ];
+var columnTwo = ["base-court","bat-fowling","beef-witted","beetle-headed","boil-brained","clapper-clawed","clay-brained","common-kissing","crook-pated","dismal-dreaming","dizzy-eyed","doghearted","dread-bolted","earth-vexing","elf-skinned","fat-kidneyed","fen-sucked","flap-mouthed","fly-bitten","folly-fallen","fool-born","full-gorged","guts-griping","half-faced","hasty-witted","hedge-born","hell-hated","idle-headed","ill-breeding","ill-nurtured","knotty-pated","milk-livered","motley-minded","onion-eyed","plume-plucked","pottle-deep","pox-marked","reeling-ripe","rough-hewn","rude-growing","rump-fed","shard-borne","sheep-biting","spur-galled","swag-bellied","tardy-gaited","tickle-brained","toad-spotted","unchin-snouted","weather-bitten"];
+var columnThree = ["apple-john","baggage","barnacle","bladder","boar-pig","bugbear","bum-bailey","canker-blossom","clack-dish","clotpole","coxcomb","codpiece","death-token","dewberry","flap-dragon","flax-wench","flirt-gill","foot-licker","fustilarian","giglet","gudgeon","haggard","harpy","hedge-pig","horn-beast","hugger-mugger","joithead","lewdster","lout","maggot-pie","malt-worm","mammet","measle","minnow","miscreant","moldwarp","mumble-news","nut-hook","pigeon-egg","pignut","puttock","pumpion","ratsbane","scut","skainsmate","strumpet","varlot","vassal","whey-face","wagtail"];
+
+window.onload = function () {
+  render(generate(),generate());
+};
+
+var random = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function generate() {
+  var r = columnOne[random(0,columnOne.length-1)] + " ";
+  r += columnTwo[random(0,columnTwo.length-1)] + " ";
+  r += columnThree[random(0,columnThree.length-1)];
+  return r;
+}
+
+function render(insult, insult2) {
+  document.getElementById('insult').innerHTML = "Thou " + insult + "!";
+  document.getElementById('me').innerHTML = "Made by the " + insult2 + " " + '<a href=\"https://joelgritter.github.io\">Joel Gritter</a>';
+}
